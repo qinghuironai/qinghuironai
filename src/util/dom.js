fb7a3c09b45cd2fc846bbc709488cc9029812664
Ariussssss <1059146811@qq.com>
2019-10-09 03:37:10 +0800

    fix: downloading dialog

--- src/util/dom.js
+++ src/util/dom.js
@@ -2,7 +2,7 @@
  * @Author: Arius
  * @Date:   2019-10-05 05:32:08
  * @Last Modified by:   Arius
- * @Last Modified time: 2019-10-08 16:43:18
+ * @Last Modified time: 2019-10-09 03:26:10
  */
 
 function isInViewPortOfOne (el, pad = 100) {
@@ -16,15 +16,16 @@ function isInViewPortOfOne (el, pad = 100) {
   return top <= viewPortHeight + pad
 }
 
-function download (href) {
+function download (href, cb) {
   const eleLink = document.createElement('a')
   eleLink.download = ''
   eleLink.href = href
   eleLink.click()
   eleLink.remove()
+  cb && cb()
 }
 
-function downloadByData (url) {
+function downloadByData (url, cb) {
   const image = new Image()
   image.setAttribute('crossOrigin', 'anonymous')
   image.src = url
@@ -34,15 +35,13 @@ function downloadByData (url) {
     canvas.height = image.height
     const ctx = canvas.getContext('2d')
     ctx.drawImage(image, 0, 0, image.width, image.height)
-    // const ext = image.src.substring(image.src.lastIndexOf('.')+1).toLowerCase()
-    const ext = 'png'
+    const ext = image.src.substring(image.src.lastIndexOf('.') + 1).toLowerCase()
     const dataURL = canvas.toDataURL('image/' + ext)
-    console.log(ext, image.width, image.height)
-    download(dataURL)
+    download(dataURL, cb)
   }
 }
 
-function downloadByBlob (url) {
+function downloadByBlob (url, cb) {
   let image = new Image()
   image.setAttribute('crossOrigin', 'anonymous')
   image.src = url
@@ -54,7 +53,7 @@ function downloadByBlob (url) {
     ctx.drawImage(image, 0, 0, image.width, image.height)
     canvas.toBlob((blob) => {
       let url = URL.createObjectURL(blob)
-      download(url)
+      download(url, cb)
       // 用完释放URL对象
       URL.revokeObjectURL(url)
     })
