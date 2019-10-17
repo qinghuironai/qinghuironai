7e5c3b08384753952abb4eaaf295581f74954c74
dongzy <dongzy@cadg.cn>
2019-10-17 17:34:39 +0800

    ｆｉｘ　ｓｏｍｅ　ｔｈｉｎｇ

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,20 +1,35 @@
 <template>
   <div :class="['text-input__wrapper', { typing }]">
-    <img src="../assets/images/cat.png" alt="cat">
+    <img
+      alt="cat"
+      src="../assets/images/cat.png"
+      v-show="showCat"
+    />
     <input
-      class="text-input"
-      v-model="value"
       :placeholder="placeholder"
       @input="change"
-      @keyup.enter="enter"/>
-    <img src="../assets/images/camera.svg" alt="camera">
-    <img src="../assets/images/search.svg" @click="enter" alt="search">
-    <div class="text-input__keyword" v-if="typing">
+      @keyup.enter="enter"
+      class="text-input"
+      v-model="value"
+    />
+    <img
+      alt="camera"
+      src="../assets/images/camera.svg"
+    />
+    <img
+      @click="enter"
+      alt="search"
+      src="../assets/images/search.svg"
+    />
+    <div
+      class="text-input__keyword"
+      v-if="typing"
+    >
       <router-link
-        class="text-input__keyword--item"
-        v-for="(item, index) in keywords"
         :key="index"
         :to="{ name: 'PopSearch', query: { keyword: item } }"
+        class="text-input__keyword--item"
+        v-for="(item, index) in keywords"
       >
         <p>{{ item }}</p>
       </router-link>
@@ -35,6 +50,10 @@ export default {
     placeholder: {
       default: `(●'◡'●)ﾉ關鍵字の輸入`,
       type: String
+    },
+    showCat: {
+      default: false,
+      type: Boolean
     }
   },
   data () {
@@ -63,10 +82,9 @@ export default {
     },
     getKeyword: debounce(function () {
       if (!this.typing) return
-      this.$api.search
-        .getKeyword(this.value).then(({ data: { data } }) => {
-          this.keywords = data.keywordList
-        })
+      this.$api.search.getKeyword(this.value).then(({ data: { data } }) => {
+        this.keywords = data.keywordList
+      })
     }, 500),
     blur () {
       this.typing = false
@@ -77,70 +95,92 @@ export default {
       this.$emit('enter', this.value)
     },
     redirect (val) {
-      this.$router.push({ name: 'PopSearch', query: { keyword: val } })
-        .catch(e => {
-          this.value = val
-        })
+      this.$router.push({ name: 'PopSearch', query: { keyword: val } }).catch(e => {
+        this.value = val
+      })
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-  .text-input
-    padding .8rem
-    border 0 none
-    border-radius 1rem
-    width 100%
-    background $white
-    font-size 1.4rem
-    -webkit-appearance none
-    border none
-    outline none
-    padding-right 6rem
-    box-sizing border-box
-    &__wrapper
-      position relative
-      box-sizing border-box
-      left 0
-      right 0
-      margin .8rem
-      img
-        position absolute
-        &:first-child
-          width 8rem
-          top -6.5rem
-          left .7rem
-        &:nth-of-type(2), &:nth-of-type(3)
-          width 2rem
-          height 2rem
-          top 0.5rem
-        &:nth-of-type(2)
-          right 3rem
-        &:nth-of-type(3)
-          right 0.8rem
-    img:first-child
-      margin-right 2rem
-    &__keyword
-      color black
-      border-radius .25rem
-      background white
-      overflow hidden
-      &--mask
-        position absolute
-        top -20px
-        left 0
-        padding 100px .8rem
-        height 100vh
-        z-index 1
-      &--item
-        display inline-block
-        box-sizing border-box
-        width 100%
-        font-size 20px
-        padding 0.8rem
-        color white
-        background #33a3dc4d
-        + .text-input__keyword--item
-          border-top 1px solid white
+.text-input {
+  padding: 0.8rem;
+  border: 0 none;
+  border-radius: 1rem;
+  width: 100%;
+  background: white;
+  font-size: 1.4rem;
+  -webkit-appearance: none;
+  border: none;
+  outline: none;
+  padding-right: 6rem;
+  box-sizing: border-box;
+
+  &__wrapper {
+    position: relative;
+    box-sizing: border-box;
+    left: 0;
+    right: 0;
+    margin: 0.8rem;
+
+    img {
+      position: absolute;
+
+      &:first-child {
+        width: 8rem;
+        top: -6.5rem;
+        left: 0.7rem;
+      }
+
+      &:nth-of-type(2), &:nth-of-type(3) {
+        width: 2rem;
+        height: 2rem;
+        top: 0.5rem;
+      }
+
+      &:nth-of-type(2) {
+        right: 3rem;
+      }
+
+      &:nth-of-type(3) {
+        right: 0.8rem;
+      }
+    }
+  }
+
+  img:first-child {
+    margin-right: 2rem;
+  }
+
+  &__keyword {
+    color: black;
+    border-radius: 0.25rem;
+    background: white;
+    overflow: hidden;
+
+    &--mask {
+      position: absolute;
+      top: -20px;
+      left: 0;
+      padding: 100px 0.8rem;
+      height: 100vh;
+      z-index: 1;
+    }
+
+    &--item {
+      display: inline-block;
+      box-sizing: border-box;
+      width: 100%;
+      font-size: 20px;
+      padding: 0.8rem;
+      color: white;
+      background: white;
+
+      + .text-input__keyword--item {
+        border-top: 1px solid white;
+      }
+    }
+  }
+}
 </style>
