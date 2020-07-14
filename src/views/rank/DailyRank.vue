1ba28dfa7f814a5b0bcf119d7e9e477c3f514024
OysterQAQ <oysterqaq@gmail.com>
2020-07-14 17:00:33 +0800

    change apk download url

--- src/views/rank/DailyRank.vue
+++ src/views/rank/DailyRank.vue
@@ -11,7 +11,7 @@
     >
       <Header @selectMode="selectMode" @selectDate="selectDate" />
     </List>
-    <a v-if="isAndroid" class="download" href="https://wwa.lanzous.com/icnKYe59x4f" download="pixivic">下载App</a>
+    <a v-if="isAndroid" class="download" href="https://wwa.lanzous.com/iR9E5eljotc" download="pixivic">下载App</a>
     <a v-if="isiOS" class="download" href="https://apps.apple.com/cn/app/pixivic/id1508873995">下载App</a>
   </div>
 </template>
