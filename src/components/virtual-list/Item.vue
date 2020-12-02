c319644d403a35122755080a5d7c5f0d4f159b56
OysterQAQ <oysterqaq@gmail.com>
2020-12-02 21:44:14 +0800

    添加广告追踪

--- src/components/virtual-list/Item.vue
+++ src/components/virtual-list/Item.vue
@@ -51,6 +51,11 @@ export default {
     },
     goDetail() {
       if (this.column.isad) {
+            this.$gtag.event( 'click', {
+                'event_category': 'outbound',
+                'event_label': this.column.link,
+                'transport_type': 'beacon'
+              });
         window.open(this.column.link);
       } else {
         this.$store.dispatch('setDetail', this.column);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import animated from 'animate.css';
 import App from './App.vue';
+import VueGtag from "vue-gtag";
 import router from './router';
 import store from './store/';
 import api from './api';
@@ -34,7 +35,11 @@ Vue.filter('replaceBig', val => replaceBigImg(val));
 Vue.filter('replaceSmall', val => replaceSmallImg(val));
 Vue.filter('replaceAvatar', val => `https://static.pixivic.net/avatar/299x299/${val}.jpg?${store.getters.avatar.split('?')[1]}`);
 Vue.filter('replaceSquare', val => 'https://img.cheerfun.dev/c/360x360_70/img-master' + val.split('img-master')[1]);
-
+Vue.use(VueGtag, {
+  config: {
+    id: "UA-158701012-1"
+  }
+});
 const app = new Vue({
   router,
   store,
