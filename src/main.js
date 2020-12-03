4957a842c1049045359c0c9adabf9ed5d54410ec
OysterQAQ <oysterqaq@gmail.com>
2020-12-03 12:39:48 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ Vue.filter('dateFormat', val => {
 });
 Vue.filter('replaceBig', val => replaceBigImg(val));
 Vue.filter('replaceSmall', val => replaceSmallImg(val));
-Vue.filter('replaceAvatar', val => `https://static.pixivic.net/avatar/299x299/${val}.jpg`);
+Vue.filter('replaceAvatar', val => `https://static.pixivic.net/avatar/299x299/${val}.jpg?${store.getters.avatar!=null?store.getters.avatar.split('?')[1]:''}`);
 Vue.filter('replaceSquare', val => 'https://img.cheerfun.dev/c/360x360_70/img-master' + val.split('img-master')[1]);
 const app = new Vue({
   router,
